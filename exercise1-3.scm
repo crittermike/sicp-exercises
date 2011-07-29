9e402a42e58e8f9e0706830dce34b9902b3331b4
Mike Crittenden <mikecrittenden@gmail.com>
2011-07-29 10:28:33 -0400

    initial commit

---
+++ exercise1-3.scm
@@ -0,0 +1,6 @@
+(define (square x) (* x x))
+(define (sum-of-squares x y) (+ (square x) (square y)))
+(define (sum-of-squares-two-largest x y z) (cond ((and (> x z) (> y z)) (sum-of-squares x y))
+                          ((and (>= x y) (>= z y)) (sum-of-squares x z))
+                          ((and (>= y x) (>= z x)) (sum-of-squares y z))))
+(print (sum-of-squares-two-largest 4 5 4))
