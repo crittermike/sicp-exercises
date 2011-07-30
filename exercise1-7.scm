07dd45e7dff095bdd43172b12e2e13bf53f2210a
Mike Crittenden <mikecrittenden@gmail.com>
2011-07-29 22:07:20 -0400

    working exercise 1.7

--- exercise1-7.scm
+++ exercise1-7.scm
@@ -1,9 +1,8 @@
-(define (sqrt-iter guess x change)
-  (if good-enough? change x)
-    guess
-    (sqrt-iter (improve guess x) 
-               x 
-               (- (improve guess x) guess)))
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
 
 (define (improve guess x)
   (average guess (/ x guess)))
@@ -14,11 +13,11 @@
 (define (square x)
   (* x x))
 
-(define (good-enough? change x)
-  (< (abs (/ change x)) 10))
+(define (good-enough? guess x)
+    (< (/ (abs (- guess (improve guess x))) x) 0.0000001))
 
 (define (sqrt x)
-  (sqrt-iter 1.0 x 1.0))
+  (sqrt-iter 1.0 x))
 
 
-(print (square (sqrt 9)))
+(print (sqrt 9000000))
